5b632d529f1306da3ed187c2b8b5cf8ac2fc623d
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2023-03-24 05:18:15 +0200

    fix: Fix exported CustomEvent type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { CustomEvent as _CustomEvent } from "./customEvent";
+export { CustomEvent } from "./customEvent";
 export { on, dispatch } from "./core";
 export { globalEventBus as eventBus } from "./eventBus";
 
-export type CustomEvent = typeof _CustomEvent;
