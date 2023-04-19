6290b1854a28d2708a646434ec64c1ce15a402d2
Dimitris Klouvas <dimtris.klouvas@gmail.com>
2023-04-20 01:29:32 +0300

    chore: Refactor core to small functions

--- src/core.ts
+++ src/core.ts
@@ -2,6 +2,18 @@ import { debounce } from "./utils";
 import { globalEventBus } from "./eventBus";
 import { CustomEvent } from "./customEvent";
 
+type DecoratorType = (
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) => any;
+
+type DecoratorReturnFnType = (
+  target: any,
+  propertyKey?: string,
+  descriptor?: PropertyDescriptor
+) => any;
+
 type BaseParams = {
   eventName?: string;
   eventBus?: EventTarget;
@@ -21,24 +33,13 @@ export const dispatch = ({
   eventBus = globalEventBus,
 }: DispatchParams = {}) => {
 
-  const dispatcher = (
-    target: any,
-    propertyKey: string,
-    propertyDescriptor: PropertyDescriptor
-  ) => {
-    const isInstanceMethod = !target.prototype;
-    const namespace = (
-      isInstanceMethod ? target.constructor : target
-    ).name.toLowerCase();
-    const generatedEventName = [namespace, propertyKey]
-      .filter(Boolean)
-      .join(":");
-    const event = eventName || generatedEventName;
+  const dispatcher: DecoratorType = (target, propertyKey, descriptor) => {
+    const event = eventName || generateEventName(target, propertyKey, "");
 
-    const wrapperFn = (...args: any[]): typeof propertyDescriptor.value => {
+    const wrapperFn = (...args: any[]): typeof descriptor.value => {
       // When static method use null and when instance method use this
-      const applyTarget = isInstanceMethod ? this : null;
-      const result = propertyDescriptor.value.apply(applyTarget, args);
+      const applyTarget = isInstanceMethod(target) ? this : null;
+      const result = descriptor.value.apply(applyTarget, args);
       Promise.resolve(result).then((data) => {
         eventBus.dispatchEvent(new CustomEvent(event, { data }));
       });
@@ -46,7 +47,7 @@ export const dispatch = ({
     };
 
     // When static method use target and when instance method use this
-    const propertyTarget = isInstanceMethod ? this : target;
+    const propertyTarget = isInstanceMethod(target) ? this : target;
     Object.defineProperty(propertyTarget, propertyKey, {
       value: wrapperFn,
       configurable: true,
@@ -56,48 +57,26 @@ export const dispatch = ({
     return wrapperFn;
   };
 
-  const dispatchMethod = (
-    target: any,
-    propertyKey: string,
-    propertyDescriptor: PropertyDescriptor
-  ) => {
+  const dispatchMethod: DecoratorType = (...args) => {
     return {
       get() {
-        return dispatcher(target, propertyKey, propertyDescriptor);
+        return dispatcher(...args);
       }
     }
   };
 
   const dispatchClass = (constructor: any) => {
-    const staticMethodNames = Object.getOwnPropertyNames(constructor).filter(
-      (m) => !["constructor", "length", "name", "prototype"].includes(m)
-    );
-
-    updateDescriptors(staticMethodNames, constructor, dispatcher);
-
-    const instanceMethodNames = Object.getOwnPropertyNames(
-      constructor.prototype
-    ).filter((m) => !["constructor"].includes(m));
-    updateDescriptors(
-      instanceMethodNames,
-      constructor.prototype,
-      dispatcher
-    );
-
+    decorateFnToAllMethods(constructor, dispatcher);
     return constructor;
   };
 
-  return (
-    target: any,
-    propertyKey?: string,
-    propertyDescriptor?: PropertyDescriptor
-  ) => {
-    if (propertyKey && propertyDescriptor) {
-      return dispatchMethod(target, propertyKey, propertyDescriptor);
-    }
-
-    return dispatchClass(target);
+  const returnFn: DecoratorReturnFnType = (target, propertyKey, descriptor) => {
+    return propertyKey && descriptor
+      ? dispatchMethod(target, propertyKey, descriptor)
+      : dispatchClass(target);
   };
+
+  return returnFn;
 };
 
 export const on = ({
@@ -108,33 +87,18 @@ export const on = ({
   debounceMs,
 }: OnParams = {}) => {
   const onClass = (constructor: any) => {
-    const staticMethodNames = Object.getOwnPropertyNames(constructor).filter(
-      (m) => !["constructor", "length", "name", "prototype"].includes(m)
-    );
-    updateDescriptors(staticMethodNames, constructor, onMethod);
-
-    const instanceMethodNames = Object.getOwnPropertyNames(
-      constructor.prototype
-    ).filter((m) => !["constructor"].includes(m));
-    updateDescriptors(instanceMethodNames, constructor.prototype, onMethod);
+    decorateFnToAllMethods(constructor, onMethod);
+    return constructor;
   };
 
-  const onMethod = (
-    target: any,
-    propertyKey: string,
-    propertyDescriptor: PropertyDescriptor
-  ) => {
-    const isInstanceMethod = !target.prototype;
-    const namespace = (isInstanceMethod ? target.constructor : target).name
-      .replace(/Handler/, "")
-      .toLowerCase();
-    const event = eventName || `${namespace}:${propertyKey}`;
+  const onMethod: DecoratorType = (target, propertyKey, descriptor) => {
+    const event = eventName || generateEventName(target, propertyKey, "Handler");
 
     let eventHandler = (evt: unknown) => {
       try {
         // When static method use null and when instance method use this
-        const applyTarget = isInstanceMethod ? this : null;
-        propertyDescriptor.value.apply(applyTarget, [evt]);
+        const applyTarget = isInstanceMethod(target) ? this : null;
+        descriptor.value.apply(applyTarget, [evt]);
       } catch (err) {
         if (errorHandler) {
           errorHandler(err as Error);
@@ -156,32 +120,49 @@ export const on = ({
     }
   };
 
-  return (
-    target: any,
-    propertyKey?: string,
-    propertyDescriptor?: PropertyDescriptor
-  ) => {
-    if (propertyKey && propertyDescriptor) {
-      return onMethod(target, propertyKey, propertyDescriptor);
-    }
-
-    return onClass(target);
+  const returnFn: DecoratorReturnFnType = (target, propertyKey, descriptor) => {
+    return propertyKey && descriptor
+      ? onMethod(target, propertyKey, descriptor)
+      : onClass(target);
   };
+
+  return returnFn;
 };
 
-const updateDescriptors = (
-  methodNames: string[],
-  target: any,
-  handler: (t: any, m: string, p: PropertyDescriptor) => unknown
-) => {
-  methodNames.forEach((methodName) => {
-    const propertyDescriptor = Object.getOwnPropertyDescriptor(
-      target,
-      methodName
-    );
-    /* istanbul ignore next */
-    if (!propertyDescriptor) return;
+type DecorateHandlerType = (t: any, m: string, p: PropertyDescriptor) => unknown;
 
-    target[methodName] = handler(target, methodName, propertyDescriptor);
-  });
+const decorateMethod = (target: any, methodName: string, handler: DecorateHandlerType) => {
+  const propertyDescriptor = Object.getOwnPropertyDescriptor(target, methodName);
+  /* istanbul ignore next */
+  if (!propertyDescriptor) return;
+
+  target[methodName] = handler(target, methodName, propertyDescriptor);
+}
+
+const decorateFnToAllMethods = (constructor: any, handler: DecorateHandlerType) => {
+  getStaticMethods(constructor).forEach(methodName => decorateMethod(constructor, methodName, handler));
+  getInstanceMethods(constructor).forEach(methodName => decorateMethod(constructor.prototype, methodName, handler));
 };
+
+const isInstanceMethod = (target: any): boolean => {
+  return !target.prototype;
+}
+
+const getNamespace = (target: any, suffix: string): string => {
+  const Fn = isInstanceMethod(target) ? target.constructor : target;
+  return Fn.name.replace(suffix, "").toLowerCase();
+}
+
+const generateEventName = (target: any, propertyKey: string, suffix: string): string => {
+  return [getNamespace(target, suffix), propertyKey].filter(Boolean).join(":");
+}
+
+const getStaticMethods = (constructor: Function): string[] => {
+  return Object.getOwnPropertyNames(constructor).filter(
+    (m) => !["constructor", "length", "name", "prototype"].includes(m)
+  );
+}
+
+const getInstanceMethods = (constructor: Function): string[] => {
+  return Object.getOwnPropertyNames(constructor.prototype).filter((m) => !["constructor"].includes(m));
+}
